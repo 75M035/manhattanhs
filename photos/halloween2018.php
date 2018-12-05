6a308cec1d5ed8a4c6ed76a5a4491d2d123e9b2b
Asher <aroberts16@schools.nyc.gov>
2018-12-05 09:40:25 -0500

    Fixing Joseph's bugs

--- photos/halloween2018.php
+++ photos/halloween2018.php
@@ -119,8 +119,6 @@ function showDivs(n) {
 				<div class="column">
 					<img class="gallerythumbnail" src="/images/photos/2018_2019/halloween2018/halloween2018_beacon_7th.jpg" alt="Halloween 6" style="width:100%" onclick="currentDiv(6)">
 				</div>
-				
-			</div>
 			</div>
 			
 		<script>
