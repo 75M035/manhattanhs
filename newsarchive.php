c90af0c8ada4979f5a910ef765b62743c6ac86f9
joseph719 <planer.joseph@yahoo.com>
2019-04-02 07:23:02 -0400

    Added archive, updated upcoming and reminders

---
+++ newsarchive.php
@@ -0,0 +1,18 @@
+<?php 
+$page_title = "News Archive";
+$page_description = "Manhattan High School News Archive";
+
+include("includes/header.php");
+?>
+	<main>
+		<div id="fullwidth">
+			<h3 class="contenthead">News Archive</h3>
+			<div class="fullcontent">
+				<table>
+					<tr><td><a href="news2017.php">2017 News</a></td></tr>
+				</table>
+			</div>
+		</div>
+	</main>
+
+<?php include("includes/footer.php");?>
