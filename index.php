3b30797192710819e8f7b974b10427d28bb861c8
joseph719 <jplaner@schools.nyc.gov>
2020-08-02 11:27:01 -0400

    *Updated upcoming

--- index.php
+++ index.php
@@ -209,7 +209,6 @@ include("includes/header.php");
 				<h2 class="contentheadright">Upcoming</h2>
 				<div class="sidecontent upcoming">
 					<p><span class="all-label">All</span> <span class="upcoming-date">03/17/2020-08/13/2020</span> <br /> School Closed for Instruction (Remote Instruction)</p>
-					<p><span class="all-label">All</span> <span class="upcoming-date">07/31/2020</span> <br /> Eid al-Adha (school closed)</p>
 					<p><span class="all-label">All</span> <span class="upcoming-date">08/13/2020</span> <br /> Last Day of Summer School</p>
 				</div>
 			</aside>
