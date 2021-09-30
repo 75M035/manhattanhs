ed0a7a943c31f2514486d7a1add93d3a18e10850
jplaner-h <84940309+jplaner-h@users.noreply.github.com>
2021-09-30 08:30:28 -0400

    Updated place ID for Maps API on bellevue.php
    Updated place ID for Maps API on nyspi.php

--- nyspi.php
+++ nyspi.php
@@ -13,7 +13,7 @@ include("includes/header.php");
 				<div class="about-image"><img src="images/about/aboutnyspi.jpg" alt="NYS Psychiatric Institute"></div>
 				<p>P35M’s program at the New York State Psychiatric Institute provides school services to students receiving outpatient treatment at the hospital’s Children’s Day Unit (CDU). It is a 12:1:1 high school program inside a hospital unit. The CDU is a research program that provides free services, including therapy and medication, to 14-21-year-old high-risk youth with emotional disabilities, such as anxiety, depression, and eating disorders.</p>
 				<p>Many different types of students on different academic levels attend this program. Our staff work with the students and their original schools to allow them to continue their education while receiving treatment and ensure they do not fall behind. Our services are year-round, allowing students to make up classes in the summer as well when necessary.</p>
-				<div class="google-maps"><iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJT2WxyJv2wokREy-XPiOmgQM&key=AIzaSyB_kD5TwtDPL8zde3GbJXGQaauEhQ0GMK8" title="Google Maps" allowfullscreen></iframe></div>
+				<div class="google-maps"><iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJY7onaOH3wokRK2q2s0AoeQ4&key=AIzaSyB_kD5TwtDPL8zde3GbJXGQaauEhQ0GMK8" title="Google Maps" allowfullscreen></iframe></div>
 			</div>
 		</section>
 	</div>
