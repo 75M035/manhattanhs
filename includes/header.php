38ddd14f0f6a4be0c6137f378d786ab484316854
aroberts16 <44061440+aroberts16@users.noreply.github.com>
2023-11-21 10:03:07 -0500

    Fixing header color

--- includes/header.php
+++ includes/header.php
@@ -30,9 +30,9 @@
 <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon/favicon-96x96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
 <link rel="manifest" href="/images/favicon/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileColor" content="#F8F3E6">
 <meta name="msapplication-TileImage" content="/images/favicon/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
+<meta name="theme-color" content="#F8F3E6">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://use.fontawesome.com/97c5943790.js"></script>
 <link rel="stylesheet" type="text/css" href="/includes/footable/footable.standalone.min.css">
